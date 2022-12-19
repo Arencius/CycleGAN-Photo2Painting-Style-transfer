5f4e560228f82391e30d0868d348e1133f0a811f
karol <karol.krawczyk1@edu.uekat.pl>
2022-12-19 22:00:56 +0100

    updated requirements.txt file

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from torchsummary import summary
+from src.model.generator import Generator
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    generator = Generator().cuda()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    summary(generator, (3, 256, 256))
